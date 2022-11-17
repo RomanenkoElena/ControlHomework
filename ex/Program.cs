94d33aa078072975d3a0b17d65344ef371a8ab14
Elena Romanenko <murenec_elena@mail.ru>
2022-11-17 13:36:58 +0600

    added ex

---
+++ ex/Program.cs
@@ -0,0 +1,29 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам.
+
+string[] array1 = new string[7] { "Hello, world!", "123", "IT", ":)", "data science", "2+2", "time" };
+string[] array2 = new string[array1.Length];
+XZ(array1,array2);
+PrintArray(array2);
+
+void XZ(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
